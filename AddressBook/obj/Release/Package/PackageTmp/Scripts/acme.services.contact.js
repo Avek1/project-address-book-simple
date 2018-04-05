fbb84eadacee284c1274a5c5bf4947d60934076e
juan f <jlfloresjr@gmail.com>
2018-04-04 22:39:11 -0700

    just pushing again.

---
+++ AddressBook/obj/Release/Package/PackageTmp/Scripts/acme.services.contact.js
@@ -0,0 +1,54 @@
+﻿acme.services.contact = acme.services.contact || {};
+
+acme.services.contact.getAll = function(onSuccess, onError){
+	var url = '/api/contact';
+	var settings = {
+		cache: false,
+		dataType: "json",
+		success: onSuccess,
+		error: onError,
+		type: 'GET'
+	};
+	$.ajax(url, settings);
+};
+
+acme.services.contact.save = function (data, onSuccess, onError) {
+	var url = '/api/contact';
+	var settings = {
+		cache: false,
+		contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+		dataType:"json",
+		type: 'POST',
+		data: data,
+		success: onSuccess,
+		error: onError
+	};
+	$.ajax(url, settings);
+};
+
+acme.services.contact.update = function (data, onSuccess, onError) {
+	var url = '/api/contact';
+	var settings = {
+		cache: false,
+		contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+		dataType: "json",
+		type: 'PUT',
+		data: data,
+		success: onSuccess,
+		error: onError
+	};
+	$.ajax(url, settings);
+};
+
+acme.services.contact.delete = function (data, onSuccess, onError) {
+	var url = '/api/contact?id=' + data;
+	var settings = {
+		cache: false,
+		contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+		dataType: "json",
+		type: 'DELETE',
+		success: onSuccess,
+		error: onError
+	};
+	$.ajax(url, settings);
+};
